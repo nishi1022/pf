60935848bf450106a5c2ecf8ee9305db8815c47a
nishi1022 <139068548+nishi1022@users.noreply.github.com>
2023-08-17 15:14:22 +0900

    upload

--- script.js
+++ script.js
@@ -1,28 +1,39 @@
 'use strict';
 
-jQuery(function($) {
+jQuery(function ($) {
 	const nav = $('#top_fixed'),
-	offset = nav.offset();
+		offset = nav.offset();
 	$(window).scroll(function () {
-	  if($(window).scrollTop() > offset.top) {
-	    nav.addClass('fixed');
-	  } else {
-	    nav.removeClass('fixed');
-	  }
+		if ($(window).scrollTop() > offset.top) {
+			nav.addClass('fixed');
+		} else {
+			nav.removeClass('fixed');
+		}
 	});
 });
 
 
 
 $(function () {
-	$('#openModal').click(function(){
+	$('#openModal').click(function () {
 		$('#modalArea').fadeIn();
 	});
-	$('#closeModal , #modalBg').click(function(){
-	  $('#modalArea').fadeOut();
+	$('#closeModal , #modalBg').click(function () {
+		$('#modalArea').fadeOut();
 	});
-  });
+});
+
+// const swiper = new Swiper('.swiper', {
+//     // Optional parameters
+//     loop: true,
+//     slidesPerView: 5,
+//     spaceBetween: 30,
+//     autoplay: {
+//         delay: 2000,
+//         disableOnInteraction: false
+//     },
+
+// });
 
 
-  
 //   Babel React
